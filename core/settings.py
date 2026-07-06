137a14a190c30167aa7a4dbc0add4f0e7846a5b9
Le Van Ninh <levanninh791@gmail.com>
2026-07-06 11:44:19 +0700

    feat: add django.contrib.postgres to installed apps and update requirements.txt for PyJWT

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'django.contrib.postgres',
+
     # Third party apps
     'rest_framework',
     'rest_framework.authtoken',
